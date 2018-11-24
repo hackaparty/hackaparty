db59bbd17b43a034cfd067a3b6eea8163247f21e
oskar.doerfler <oskar.doerfler@matchmycolor.com>
2018-11-24 16:45:14 +0100

    Gyroskop changes

--- client/controller/controller.events.gyro.js
+++ client/controller/controller.events.gyro.js
@@ -12,31 +12,32 @@ export default function(server) {
 
     function handleOrientation(event) {
 
-    var alpha    = event.alpha;
-    var beta     = event.beta;
-    var gamma    = event.gamma;
+        var alpha    = event.alpha;
+        var beta     = event.beta;
+        var gamma    = event.gamma;
 
-    gyroa.innerHTML = Math.abs(lastGamma - gamma) > minDiff;
+        gyroa.innerHTML = Math.abs(lastGamma - gamma) > minDiff;
 
-    gyrob.innerHTML = ""+Math.abs(lastGamma - gamma) // "alpha: "+Math.round(alpha);
-    gyroc.innerHTML = lastGamma//"beta: "+Math.round(beta);
-    gyrod.innerHTML = "gamma:"+Math.round(gamma);
+        gyrob.innerHTML = ""+Math.abs(lastGamma - gamma) // "alpha: "+Math.round(alpha);
+        gyroc.innerHTML = lastGamma//"beta: "+Math.round(beta);
+        gyrod.innerHTML = "gamma:"+Math.round(gamma);
 
-    if (Math.abs(lastGamma - gamma) > minDiff) {
-        server.send({"gamma":gamma});
+        if (Math.abs(lastGamma - gamma) > minDiff) {
+            server.send({"gamma":gamma});
+        }
+        lastGamma = gamma;
+
+        if (Math.abs(lastBeta - beta) > minDiff) {
+            server.send({"beta":beta});
+        }
+        lastBeta = beta;
+
+        if (Math.abs(lastAlpha - alpha) > minDiff) {
+            server.send({"alpha":alpha});
+        }
+        lastAlpha = alpha;
     }
-    lastGamma = gamma;
-
-    if (Math.abs(lastBeta - beta) > minDiff) {
-        server.send({"beta":beta});
-    }
-    lastBeta = beta;
-
-    if (Math.abs(lastAlpha - alpha) > minDiff) {
-        server.send({"alpha":alpha});
-    }
-    lastAlpha = alpha;
 }
 
-}
+
 
