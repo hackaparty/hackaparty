1b5eb52921ad814a71df7f02211873f715282331
patrick.winkler <patrick.winkler@matchmycolor.com>
2018-11-24 11:20:40 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const WebSocket = require('ws');
+
+const app = express();
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({port: 3001});
+let server;
+
+wss.on('connection', (ws, req) => {
+  if(req.url === '/playground'){
+    server = ws;
+  }
+  else if(req.url === '/controller') {
+    ws.on('message', (message) => {
+      if(server){
+        server.send(`${message}`);
+      }
+    });
+  }
+  else {
+    ws.destroy();
+  }
+});
+
+
+app.use('/controller', express.static('controller'));
+app.use('/playground', express.static('playground'));
+
+
+app.listen(3000, function(){
+  console.log('Listening on port 3000.');
+});
