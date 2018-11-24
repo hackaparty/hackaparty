f6980caa2ede24a143553ab88e80726d0ca1093b
James L <james.m.lowery@gmail.com>
2018-11-24 15:56:11 +0100

    passing unique client_id after submitting credentials

--- server/login/index.js
+++ server/login/index.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var router = express.Router()
 
+const uuidv4 = require('uuid/v4')
+
 // middleware that is specific to this router
 router.use(function timeLog (req, res, next) {
   console.log('Time: ', Date.now())
@@ -11,7 +13,7 @@ router.get('/', express.static('client/login'));
 
 // define the about route
 router.post('/', function (req, res) {
-  res.send('About birds')
+  res.redirect('/controller?client_id=' + uuidv4());
 })
 
 module.exports = router
