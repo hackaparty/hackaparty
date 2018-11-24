530f6e7438c532e98091776d260e9facfc857343
patrick.winkler <patrick.winkler@matchmycolor.com>
2018-11-24 20:29:06 +0100

    Moved example to games folder.

--- client/games/playground.js
+++ client/games/playground.js
@@ -12,16 +12,35 @@ class playground {
 
     initSocket() {
         this.socket = new WebSocket("ws://" + location.hostname + ":3001/controller");
-        this.socket.onopen = this.sendMessage;
-        this.socket.onmessage = this.receiveMessage
+        this.socket.onmessage = this.receiveMessage.bind(this);
     }
 
-    sendMessage({key, value}) {
-        this.socket.send()
+    sendMessage(message) {
+        this.socket.send(message);
     }
 
     receiveMessage(message) {
-
+      console.log(msg.data);
+      if(msg.data === 'left'){
+        if(this.bunny.x>0) {
+          this.bunny.x -= 10;
+        }
+      }
+      else if(msg.data === 'right'){
+        if(this.bunny.x<800) {
+          this.bunny.x += 10;
+        }
+      }
+      else if(msg.data === 'up'){
+        if(this.bunny.y > 0) {
+          this.bunny.y -= 10;
+        }
+      }
+      else if(msg.data === 'down'){
+        if(this.bunny.y < 600) {
+          this.bunny.y += 10;
+        }
+      }
     }
 
     initControls() {}
